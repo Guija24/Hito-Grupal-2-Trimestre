c2be72ca9505ccc53b44a0475d86067bb635c339
Ivan Guijarro <93186068+Guija24@users.noreply.github.com>
2023-02-15 19:11:27 +0100

    Add files via upload

---
+++ HitoGrupal.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[3]:
+
+
+df = pd.read_csv('C:/Users/ivant/Downloads/chord-progressions.csv')
+
+
+# In[4]:
+
+
+print(df.head(10))
+
+
+# In[ ]:
+
+
+
+
